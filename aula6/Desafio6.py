96c5fac049201eff531badf5bece88145af3e6f3
ANALISTA DE DADOS - BIG DATA SCIENCE 2025.1 <51134552025.1@senacrjedu.local>
2025-04-24 18:46:14 -0300

    first commit

---
+++ aula6/Desafio6.py
@@ -0,0 +1,10 @@
+while True:
+    numb1 = float(input("Digite o primeiro núemro: "))
+    numb2 = float(input("Digite o segundo número: "))
+    numb3 = float(input("Digite o terceiro número: "))
+    check = [numb1, numb2, numb3]
+    check.sort()
+    print(f"Os números recebidos em ordem crescente fica: {check}")
+    break
+
+    
