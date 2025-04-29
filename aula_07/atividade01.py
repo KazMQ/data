5f28f86b6e48f2b1d626d06fd7f48b2e0a0325c3
KazMQ <otavio.mech@hotmail.com>
2025-04-29 20:52:20 -0300

    Atividade 1

---
+++ aula_07/atividade01.py
@@ -0,0 +1,9 @@
+def cal():
+    base = int(input("Me diga o valor: "))
+    double = base*2
+    triple = base*3
+    square = base**2
+    print(f"Os valores são {double}, {triple} e {square}")
+
+cal()
+
