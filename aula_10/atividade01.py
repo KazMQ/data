8190e9b4ed5447980e0cdbf6db6c13ce853a6c9c
KazMQ <otavio.mech@hotmail.com>
2025-05-13 21:01:06 -0300

    Trabalhando com randoms

--- aula_10/atividade01.py
+++ aula_10/atividade01.py
@@ -21,18 +21,22 @@ def multi(a,b):
     print (mult)
     return mult
 
+
+
 while True:
-    numb1 = int(input("Digite o primeiro número: "))
-    numb2 = int(input("Digite o segundo número: "))
-    print("Qual operação deseja realizar?")
-    chose = int(input("1 para soma, 2 para subtrair, 3 para dividir e 4 para multiplicar"))
+    numb1 = random.randint(1,10)
+    numb2 = random.randint(1,10)
+    #numb1 = int(input("Digite o primeiro número: "))
+    #numb2 = int(input("Digite o segundo número: "))
+    print(f"Qual operação deseja realizar? Os números de vez são {numb1} e {numb2}")
+    chose = int(input("1 para soma, 2 para subtrair, 3 para dividir e 4 para multiplicar: "))
     if chose == 1:
         somar(numb1, numb2)
-    if chose == 2:
+    elif chose == 2:
         sub(numb1,numb2)
-    if chose == 3:
+    elif chose == 3:
         div(numb1, numb2)
-    if chose == 4:
+    elif chose == 4:
         multi(numb1, numb2)
     else:
         print("Operação não identificada")
